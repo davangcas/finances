2b27fdfe9d790a05046dc6e13c587741b101f375
Angel David Castro <davangcas@gmail.com>
2021-11-01 23:57:43 -0300

    Start Project

---
+++ config/db.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SQLITE = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+POSTGRES_LOCAL = {
+    
+}
